59f9117dda46e8f4364050582c2cd67898e3677f
Tomi <258220040@qq.com>
2014-01-21 13:36:23 +0800

    config

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'miliguli');
+define('DB_NAME', 'hamp7u7yw4_mg');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', 'hamp7u7yw4_mg');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '123456');
+define('DB_PASSWORD', '0e4f2cRbD2r7');
 
 /** MySQL主机 */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
